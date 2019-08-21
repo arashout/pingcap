4079b2dc92b6d27c574a689e38230b401dab1f3c
Arash Outadi <arash.out@gmail.com>
2019-08-20 23:00:56 -0700

    Onto project 2
    But cannot get version command to work

--- src/bin/kvs.rs
+++ src/bin/kvs.rs
@@ -1,13 +1,6 @@
-use clap::{App, Arg, SubCommand};
-
 use structopt::StructOpt;
 
-const APP_NAME: &str = env!("CARGO_PKG_NAME");
-const APP_VERSION: &str = env!("CARGO_PKG_VERSION");
-const APP_AUTHORS: &str = env!("CARGO_PKG_AUTHORS");
-
 #[derive(StructOpt, Debug)]
-#[structopt(name = "kvs", about = "key value store")]
 enum KVS {
     #[structopt(name = "get")]
     Get { key: String },
@@ -18,23 +11,6 @@ enum KVS {
 }
 
 fn main() {
-    let matches = App::new(APP_NAME)
-        .version(APP_VERSION)
-        .author(APP_AUTHORS)
-        .subcommand(
-            SubCommand::with_name("get")
-                .arg(Arg::with_name("key").required(true).takes_value(true)),
-        )
-        .subcommand(
-            SubCommand::with_name("set")
-                .arg(Arg::with_name("key").required(true).takes_value(true))
-                .arg(Arg::with_name("value").required(true).takes_value(true)),
-        )
-        .subcommand(
-            SubCommand::with_name("rm").arg(Arg::with_name("key").required(true).takes_value(true)),
-        )
-        .get_matches();
-
     let config = KVS::from_args();
     println!("{:#?}", config);
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,7 @@
+#![feature(try_trait)]
+
 use std::collections::HashMap;
+use std::ops::Try;
 
 pub struct KvStore {
     map: HashMap<String, String>,
@@ -10,13 +13,16 @@ impl KvStore {
             map: HashMap::new(),
         }
     }
-    pub fn get(&self, key: String) -> Option<String> {
-        self.map.get(&key).map(|v| v.to_owned())
+    pub fn get(&self, key: String) -> Result<Option<String>, std::option::NoneError> {
+        self.map.get(&key).map(|v| Some(v.to_owned())).into_result()
     }
-    pub fn remove(&mut self, key: String) {
-        self.map.remove(&key);
+    pub fn remove(&mut self, key: String) -> Result<(), std::option::NoneError>{
+        self.map.remove(&key).map(|v| ()).into_result()
     }
-    pub fn set(&mut self, key: String, value: String) {
-        self.map.insert(key, value);
+    pub fn set(&mut self, key: String, value: String) -> Result<(), std::option::NoneError> {
+        self.map.insert(key, value).map(|v| ()).into_result()
+    }
+    pub fn open(){
+        unimplemented!();
     }
 }
